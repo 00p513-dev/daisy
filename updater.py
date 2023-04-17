898cdbdd967696a261b88188e72b8a23cfbd215b
Amy King <amy@amyrom.tech>
2023-04-17 22:03:49 +0100

    Make the updater command actually update the bot

--- updater.py
+++ updater.py
@@ -9,6 +9,10 @@ import daisySecrets
 async def update_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message_args = update.message.text.split(' ')
 
+    if update.message.from_user.id not in daisySecrets.admin_users:
+        await update.message.reply_text("You aren't able to do that...")
+        return
+    
     # Open the Git repository in the current working directory
     repo = git.Repo()
 
@@ -17,14 +21,13 @@ async def update_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     remote_ref = repo.remotes.origin.refs.main
 
     # Fetch the latest changes from the remote repository
+    await update.message.reply_text("Checking for updates...")
     repo.remotes.origin.fetch()
 
     # Check if the local and remote references are the same
     if local_ref == remote_ref:
-        replyText = "The local repository is up-to-date."
+        await update.message.reply_text("The bot is up to date.")
     else:
-        replyText = "Updating..."
-        repo.remotes.origin.pull()
-        replyText = "Updating..."
-
-    await update.message.reply_text(replyText)
+        await update.message.reply_text("Updating...")
+        repo.git.pull()
+        await update.message.reply_text("Updated!")
