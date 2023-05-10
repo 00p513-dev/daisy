632609c1bca6f3923c44146e2e1a7eed36af3178
Amy King <amy@amyrom.tech>
2023-05-10 13:57:06 +0100

    rtt: Add platform status

--- rtt.py
+++ rtt.py
@@ -6,6 +6,7 @@ from requests.auth import HTTPBasicAuth
 
 import daisySecrets
 
+
 async def train_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message_args = update.message.text.split(' ')
 
@@ -13,7 +14,8 @@ async def train_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     destination = message_args[2].upper()
 
     url = f"https://api.rtt.io/api/v1/json/search/{origin}/to/{destination}"
-    response = requests.get(url, auth=HTTPBasicAuth(daisySecrets.rttuser, daisySecrets.rttpass))
+    response = requests.get(url, auth=HTTPBasicAuth(
+        daisySecrets.rttuser, daisySecrets.rttpass))
     data = response.json()
 
     try:
@@ -21,27 +23,46 @@ async def train_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     except:
         await update.message.reply_text("Something went wrong, you probably have requested a non-existent station")
         return
-    
+
     if not services:
         await update.message.reply_text("No trains available.")
         return
 
     replyText = ""
 
-    for service in services[:3]:
+    for service in services[:5]:
         time = service['locationDetail']['gbttBookedDeparture']
         operator = service['atocCode']
         dest = service['locationDetail']["destination"][0]['description']
-        try:
+        if service["serviceType"] == "train":
             platform = "Plt " + service['locationDetail']["platform"]
-        except:
-            if service["serviceType"] == "bus":
-                platform = "Bus"
-            elif service["serviceType"] == "ship":
-                platform = "Ship"
-            else:
-                platform = "Plt UNK"
-        
+
+            try:
+                if service['locationDetail']["serviceLocation"] == "AT_PLAT":
+                    platform += " (at)"
+                elif service['locationDetail']["serviceLocation"] == "APPR_PLAT":
+                    platform += " (appr)"
+                elif service['locationDetail']["serviceLocation"] == "APPR_STAT":
+                    platform += " (appr)"
+                elif service['locationDetail']["serviceLocation"] == "DEP_PREP":
+                    platform += " (dep)"
+                elif service['locationDetail']["serviceLocation"] == "DEP_READY":
+                    platform += " (dep)"
+                else:
+                    platform += " (" + service['locationDetail']["serviceLocation"] + ")"
+            except:
+                if service['locationDetail']['platformConfirmed']:
+                    platform += " (conf)"
+                else:
+                    platform += " (ind)"
+
+        elif service["serviceType"] == "bus":
+            platform = "Bus"
+        elif service["serviceType"] == "ship":
+            platform = "Ship"
+        else:
+            platform = "Plt UNK"
+
         replyText = replyText + "\n" + f"{time} {platform} [{operator}] {dest}"
 
     await update.message.reply_text(replyText)
